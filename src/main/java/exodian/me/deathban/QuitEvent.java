061049adf00381fd69b35295297dcdd377ffbfaf
Sam Pomerantz <sampomer@iu.edu>
2020-05-10 14:48:13 -0500

    Update QuitEvent.java

--- src/main/java/exodian/me/deathban/QuitEvent.java
+++ src/main/java/exodian/me/deathban/QuitEvent.java
@@ -10,7 +10,7 @@ import org.bukkit.BanList;
 public class QuitEvent implements Listener {
     @EventHandler
     public void onQuit(PlayerQuitEvent event){
-        if((event.getPlayer().getGameMode() == GameMode.SPECTATOR) && (event.getPlayer().hasPermission("banondeath.immune"))){
+        if((event.getPlayer().getGameMode() == GameMode.SPECTATOR) && (!event.getPlayer().hasPermission("banondeath.immune"))){
             event.getPlayer().getServer().getBanList(BanList.Type.NAME).addBan(event.getPlayer().getName(), "RIP", null, "Lord Moonmoon's faithful plugin"); // NAME BAN
             event.getPlayer().getServer().getLogger().info("BanOnDeath has banned " + event.getPlayer().getName());
         }

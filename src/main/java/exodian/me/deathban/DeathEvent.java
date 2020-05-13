aac79e077baedb503972acdca6e59d3e9d75e4d9
Joseph Zurowski <Dunklheit@users.noreply.github.com>
2020-05-13 00:48:51 -0400

    Fix plugin

--- src/main/java/exodian/me/deathban/DeathEvent.java
+++ src/main/java/exodian/me/deathban/DeathEvent.java
@@ -9,17 +9,25 @@ import org.bukkit.event.player.PlayerGameModeChangeEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.BanList;
 import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Team;
 
 public class DeathEvent implements Listener {
     @EventHandler
     public void onDeath(PlayerGameModeChangeEvent event){
         Player player = event.getPlayer();
-        if((player.getGameMode() == GameMode.SPECTATOR) && (!player.hasPermission("banondeath.immune"))){
+        if((event.getNewGameMode() == GameMode.SPECTATOR) && (!player.hasPermission("banondeath.immune"))){
             player.getServer().getBanList(BanList.Type.NAME).addBan(player.getName(), "RIP", null, "BanOnDeath"); // NAME BAN
             player.getServer().getLogger().info("BanOnDeath has banned " + player.getName());
-            if(!player.getServer().getScoreboardManager().getMainScoreboard().getTeam("Brothers").hasEntry(player.getName())){
-                player.getServer().broadcast(ChatColor.DARK_RED+""+ChatColor.ITALIC+"[DeathBan] "+player.getName()+" has been automatically banned.", player.getServer().BROADCAST_CHANNEL_ADMINISTRATIVE);
-                player.kickPlayer("Thanks for playing.");
+            final Team bros = player.getServer().getScoreboardManager().getMainScoreboard().getTeam("Brothers");
+            final Team leader = player.getServer().getScoreboardManager().getMainScoreboard().getTeam("Leader");
+            final String name = player.getName();
+            try {
+                if(!bros.hasEntry(name) && !leader.hasEntry(name)){
+                    player.getServer().broadcast(ChatColor.DARK_RED+""+ChatColor.ITALIC+"[DeathBan] "+name+" has been automatically banned.", player.getServer().BROADCAST_CHANNEL_ADMINISTRATIVE);
+                    player.kickPlayer("Thanks for playing.");
+                }
+            } catch (NullPointerException ex) {
+                player.getServer().getLogger().warning("Tried to ban player, but the required teams were missing");
             }
         }
     }

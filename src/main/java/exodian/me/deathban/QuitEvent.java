7e89536ab4e8c6095b466f4fb48242c1475d52f7
Sam Pomerantz <sampom100@gmail.com>
2020-05-12 22:56:18 -0500

    added an autokick for non-brothers

--- src/main/java/exodian/me/deathban/QuitEvent.java
+++ src/main/java/exodian/me/deathban/QuitEvent.java
@@ -6,13 +6,18 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.BanList;
+import org.bukkit.entity.Player;
 
 public class QuitEvent implements Listener {
     @EventHandler
     public void onQuit(PlayerQuitEvent event){
-        if((event.getPlayer().getGameMode() == GameMode.SPECTATOR) && (!event.getPlayer().hasPermission("banondeath.immune"))){
-            event.getPlayer().getServer().getBanList(BanList.Type.NAME).addBan(event.getPlayer().getName(), "RIP", null, "Lord Moonmoon's faithful plugin"); // NAME BAN
-            event.getPlayer().getServer().getLogger().info("BanOnDeath has banned " + event.getPlayer().getName());
+        Player player = event.getPlayer();
+        if((player.getGameMode() == GameMode.SPECTATOR) && (!player.hasPermission("banondeath.immune"))){
+            player.getServer().getBanList(BanList.Type.NAME).addBan(player.getName(), "RIP", null, "Lord Moonmoon's faithful plugin"); // NAME BAN
+            player.getServer().getLogger().info("BanOnDeath has banned " + player.getName());
+            if(!player.getServer().getScoreboardManager().getMainScoreboard().getTeam("Brothers").hasEntry(player.getName())){
+                player.kickPlayer("Thanks for playing.");
+            }
         }
     }
 }
